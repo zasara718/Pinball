d3ebcb81cb3de78558111de2ed90c8f1255d92f1
zasara <zasara718@gmail.com>
2019-02-28 11:38:47 +0900

    Lesson5 課題
    課題内容の作成

---
+++ Assets/Score.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+	//スコアを０で初期化する
+	int score = 0;
+	//スコアを表示するテキスト
+	private GameObject ScoreText;
+
+	// Use this for initialization
+	void Start () {
+		//シーン中のScoreオブジェクトを取得
+		this.ScoreText = GameObject.Find ("Score");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//ボールがタグ付けしたオブジェクトに触れた時scoreを加算する
+		
+	}
+
+	void OnCollisionEnter (Collision other) {
+		if (other.gameObject.tag == "SmallStarTag") {
+			this.ScoreText.GetComponent<Text> ().text = "Score" + (this.score += 10);
+		} else if (other.gameObject.tag == "LargeStarTag") {
+			this.ScoreText.GetComponent<Text> ().text = "Score" + (this.score += 20);
+		} else if (other.gameObject.tag == "SmallCloudTag") {
+			this.ScoreText.GetComponent<Text> ().text = "Score" + (this.score += 15);
+		} else if (other.gameObject.tag == "LargeCloudTag") {
+			this.ScoreText.GetComponent<Text> ().text = "Score" + (this.score += 30);
+		}
+	}
+}
---
+++ Assets/StarController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour {
+
+	//回転速度
+	private float rotSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		//回転を開始する角度を設定
+		this.transform.Rotate(0, Random.Range(0, 360), 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//回転
+		this.transform.Rotate(0, this.rotSpeed, 0);
+	}
+}
